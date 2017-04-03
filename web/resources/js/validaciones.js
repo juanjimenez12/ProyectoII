32a0020dab42048edf27dc5d0d31b02951e344e5
davidstl5 <davidstl4@gmail.com>
2017-04-03 10:03:24 -0500

    Insertadas validaciones

--- web/resources/js/validaciones.js
+++ web/resources/js/validaciones.js
@@ -1,100 +1,153 @@
-
 $(function(){
 	//agregar expresion 
+$.validator.addMethod('valCodigo',function(value,element)
+{
+    return this.optional(element) || /^[a-zA-Z0-9_]/ .test(value);
+});
+$.validator.addMethod('valNombre',function(value,element)
+{
+    return this.optional(element) || /^[\p{L}\s']+$/.test(value);
+});
+$.validator.addMethod('valApellido',function(value,element)
+{
+    return this.optional(element) || /^[\p{L}\s']+$/.test(value);
+});
+$.validator.addMethod('valCorreo',function(value,element)
+{
+    return this.optional(element) || /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(value);
+});
+$.validator.addMethod('valCohorte',function(value,element)
+{
+    var current_year = new Date().getFullYear().toString().split("");
+    var reg=new RegExp("^(200[0-9]|[0-"+current_year[0]+"][0-"+current_year[1]+"][0-"+current_year[2]+"][0-"+current_year[3]+"])$");
+    return this.optional(element) || reg.test(value);
+});
+$.validator.addMethod('valNombreTutor',function(value,element)
+{
+    return this.optional(element) || /^[\p{L}\s']+$/.test(value);
+});
 
-$.validator.addMethod('valContra',function(value,element)
-	 {
-	 	return this.optional(element) || /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/.test(value);
-	 });
-	 //hasta aqui ...................
-	
 
-		$.validator.setDefaults( {
-			submitHandler: function () {
-				alert( "submitted!" );
-			}
-		} );
+//hasta aqui ...................
+$.validator.setDefaults( {
+        submitHandler: function () {
+                alert( "submitted!" );
+        }
+} );
 
-                /* validacion de componente */
-			$( "#FormRegistrar" ).validate( {
-				rules: {
-					nombreEst: "required",
-					lastname1: "required",
-					username1: {
-						required: true,
-						minlength: 2
-					},
-					//se agrega la funcion nueva 
-					valContra: {
-						required:true,
-						valContra:true
-					},
-					//---------------------
-					confirm_password1: {
-						required: true,
-						minlength: 5,
-						equalTo: "#password1"
-					},
-					email1: {
-						required: true,
-						email: true
-					},
-					agree1: "required"
-				},
-				messages: {
-					nombreEst: "Ingrese el nombre",
-					lastname1: "Please enter your lastname",
-					username1: {
-						required: "Please enter a username",
-						minlength: "Your username must consist of at least 2 characters"
-					},
-					valContra: {
-						valContra :"Es usted estupido TONTO PENDEJO escriba bien ",
-						required: "Es usted estupido TONTO PENDEJO escriba bien "
-					},
-					confirm_password1: {
-						required: "Please provide a password",
-						minlength: "Your password must be at least 5 characters long",
-						equalTo: "Please enter the same password as above"
-					},
-					email1: "Please enter a valid email address",
-					agree1: "Please accept our policy"
-				},
-				errorElement: "em",
-				errorPlacement: function ( error, element ) {
-					// Add the `help-block` class to the error element
-					error.addClass( "help-block" );
+$( "#signupForm1" ).validate( {
+        rules: {
+                //se agrega la funcion nueva 
+                valCodigo: {
+                        required:true,
+                        minlength: 7,
+                        maxlength: 20,
+                        valCodigo:true
+                	},
+                valNombre: {
+                        required:true,
+                        minlength: 3,
+                        maxlength: 20,
+                        valNombre:true
+                        },
+                valApellido: {
+                        required:true,
+                        minlength: 6,
+                         maxlength: 20,
+                        valApellido:true
+                },
+                valCorreo: {
+                        required:true,	
+                        minlength: 10,
+                        maxlength: 30,					
+                        valCorreo:true
+                },
+                valCohorte: {
+                        required:true,
+                        valCohorte:true
+                },
+                valNombreTutor: {
+                        required:true,
+                        minlength: 3,
+                        maxlength: 45,
+                        valNombreTutor:true
+                },
+                agree1: "required"
+        },
+        messages: {
+                valCodigo: {
+                        valCodigo :"El código puede contener letras, números, guión bajo y su longitud debe ser de 7 a 20 caracteres",
+                        minlength: "El código puede contener letras, números, guión bajo y su longitud debe ser de 7 a 20 caracteres",
+                        maxlength: "El código puede contener letras, números, guión bajo y su longitud debe ser de 7 a 20 caracteres",
+                        required: "El código es obligatorio"
+                },
+                valNombre: {
+                        valNombre :"El nombre solo puede contener letras y su longitud debe ser de 3 a 20 caracteres",
+                        minlength: "El nombre solo puede contener letras y su longitud debe ser de 3 a 20 caracteres",
+                        maxlength: "El nombre solo puede contener letras y su longitud debe ser de 3 a 20 caracteres",
+                        required: "El nombre del estudiante es obligatorio"
+                },
+                valApellido: {
+                        valApellido :"El apellido solo puede contener letras y su longitud debe ser de 6 a 20 caracteres",
+                        minlength: "El apellido solo puede contener letras y su longitud debe ser de 6 a 20 caracteres",
+                        maxlength: "El apellido solo puede contener letras y su longitud debe ser de 6 a 20 caracteres",
+                        required: "El apellido del estudiante es obligatorio"
+                },
+                valCorreo: {
+                        valCorreo :"El formato de este correo es incorrecto y su longitud debe ser de 10 a 30 caracteres",
+                        minlength: "El formato de este correo es incorrecto y su longitud debe ser de 10 a 30 caracteres",
+                        maxlength: "El formato de este correo es incorrecto y su longitud debe ser de 10 a 30 caracteres",
+                        required: "El correo del estudiante es obligatorio"
+                },
+                valCohorte: {
+                        valCohorte :"El cohorte debe ser un año válido, cuyo valor sea mayor o igual a 2000 y menor o igual al año actual",
+                        minlength: "El cohorte debe ser un año válido, cuyo valor sea mayor o igual a 2000 y menor o igual al año actual",
+                        maxlength: "El cohorte debe ser un año válido, cuyo valor sea mayor o igual a 2000 y menor o igual al año actual",
+                        required: "El valCohorte es obligatorio"
+                },
+                valNombreTutor: {
+                        valNombreTutor :"El nombre del tutor solo puede contener letras y su longitud debe ser de 3 a 45 caracteres",
+                        minlength: "El nombre del tutor solo puede contener letras y su longitud debe ser de 3 a 45 caracteres",
+                        maxlength: "El nombre del tutor solo puede contener letras y su longitud debe ser de 3 a 45 caracteres",
+                        required: "El nombre del tutor es obligatorio"
+                },
+                agree1: "Please accept our policy"
+        },
+        errorElement: "em",
+        errorPlacement: function ( error, element ) {
+                // Add the `help-block` class to the error element
+                error.addClass( "help-block" );
 
-					// Add `has-feedback` class to the parent div.form-group
-					// in order to add icons to inputs
-					element.parents( ".col-sm-5" ).addClass( "has-feedback" );
+                // Add `has-feedback` class to the parent div.form-group
+                // in order to add icons to inputs
+                element.parents( ".col-sm-5" ).addClass( "has-feedback" );
 
-					if ( element.prop( "type" ) === "checkbox" ) {
-						error.insertAfter( element.parent( "label" ) );
-					} else {
-						error.insertAfter( element );
-					}
+                if ( element.prop( "type" ) === "checkbox" ) {
+                        error.insertAfter( element.parent( "label" ) );
+                } else {
+                        error.insertAfter( element );
+                }
 
-					// Add the span element, if doesn't exists, and apply the icon classes to it.
-					if ( !element.next( "span" )[ 0 ] ) {
-						$( "<span class='glyphicon glyphicon-remove form-control-feedback'></span>" ).insertAfter( element );
-					}
-				},
-				success: function ( label, element ) {
-					// Add the span element, if doesn't exists, and apply the icon classes to it.
-					if ( !$( element ).next( "span" )[ 0 ] ) {
-						$( "<span class='glyphicon glyphicon-ok form-control-feedback'></span>" ).insertAfter( $( element ) );
-					}
-				},
-				highlight: function ( element, errorClass, validClass ) {
-					$( element ).parents( ".col-sm-5" ).addClass( "has-error" ).removeClass( "has-success" );
-					$( element ).next( "span" ).addClass( "glyphicon-remove" ).removeClass( "glyphicon-ok" );
-				},
-				unhighlight: function ( element, errorClass, validClass ) {
-					$( element ).parents( ".col-sm-5" ).addClass( "has-success" ).removeClass( "has-error" );
-					$( element ).next( "span" ).addClass( "glyphicon-ok" ).removeClass( "glyphicon-remove" );
-				}
-			} );
+                // Add the span element, if doesn't exists, and apply the icon classes to it.
+                if ( !element.next( "span" )[ 0 ] ) {
+                        $( "<span class='glyphicon glyphicon-remove form-control-feedback'></span>" ).insertAfter( element );
+                }
+        },
+        success: function ( label, element ) {
+                // Add the span element, if doesn't exists, and apply the icon classes to it.
+                if ( !$( element ).next( "span" )[ 0 ] ) {
+                        $( "<span class='glyphicon glyphicon-ok form-control-feedback'></span>" ).insertAfter( $( element ) );
+                }
+        },
+        highlight: function ( element, errorClass, validClass ) {
+                $( element ).parents( ".col-sm-5" ).addClass( "has-error" ).removeClass( "has-success" );
+                $( element ).next( "span" ).addClass( "glyphicon-remove" ).removeClass( "glyphicon-ok" );
+        },
+        unhighlight: function ( element, errorClass, validClass ) {
+                $( element ).parents( ".col-sm-5" ).addClass( "has-success" ).removeClass( "has-error" );
+                $( element ).next( "span" ).addClass( "glyphicon-ok" ).removeClass( "glyphicon-remove" );
+        }
+} );
 
-		});
+});
 		
